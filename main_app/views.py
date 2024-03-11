c5fa436c8af7cb80babb603b3d2533a58dcba334
adrimsaav <adrimsaav@gmail.com>
2024-03-11 01:26:36 -0400

    create signup form and model

--- main_app/views.py
+++ main_app/views.py
@@ -2,3 +2,20 @@ from django.shortcuts import render
 
 def home(request):
   return render(request, 'home.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+
+def logout(request):
+    return redirect('home')
