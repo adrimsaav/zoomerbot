c1ea5ccf806be5996637c8fb37e64556c0e75952
adrimsaav <adrimsaav@gmail.com>
2024-03-14 22:02:53 -0400

    update import statement

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout, login
+import uuid
+import boto3
+import os 
 
 def home(request):
   return render(request, 'home.html')
