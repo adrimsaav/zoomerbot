6efc8178c1a03014443398956f1bf66343ba7270
Nisal <nisatt11@gmail.com>
2024-03-16 12:14:49 -0400

    add ai model

--- main_app/ai.py
+++ main_app/ai.py
@@ -1,8 +1,11 @@
+# Had to change to version 3.10 for line 2 to work
 from dotenv import load_dotenv
 load_dotenv()
 
 import os
+import streamlit as st
+from vertexai.preview.generative_models import GenerativeModel
 
+GenerativeModel.configure(api_key=os.getenv('API_KEY'))
 
-
-
+model = GenerativeModel('gemini-1.0-pro')
